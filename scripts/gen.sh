5f6269141a6614773f1dade433422e5c8a94a42f
Ryan Oldford <ryan.oldford@gmail.com>
2020-02-18 11:57:37 -0800

    Changed script to make word lists with apostrophes

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -31,5 +31,12 @@ sort -u words.txt -o words.txt
 # words_alpha (only alphabet chars, no numbers or punctuation)
 grep -v '[[:digit:]|[:punct:]]' words.txt > words_alpha.txt
 python3 scripts/create_json.py words_alpha.txt > words_dictionary.json
+# words_alpha_apos (words_alpha, but apostrophes are allowed)
+cp words_alpha.txt temp.txt
+grep "'" words.txt >> temp.txt
+grep -v '-' temp.txt > words_alpha_apos.txt
+rm temp.txt
+python3 scripts/create_json.py words_alpha_apos.txt > words_dictionary_apos.json
+# zip files
 rm *.zip
 find . -type f -name "words*" -maxdepth 1 -execdir sh -c 'zip "${0%.*}.zip" "$0"' {} \;
